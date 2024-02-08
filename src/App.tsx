b1eeba5ff11e99a88a6223b94d87d1e434cda004
LuiiVuitton <Luis1134@icloud.com>
2024-02-08 12:07:08 -0500

    navbar update

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import "./index.css";
 import NavBar from "./components/NavBar";
 
 function App() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{html,js}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
     extend: {},
+    colors: {
+      navBarColor: "#020617",
+      navBarText: "#f1f5f9",
+      navBarHover: "##dc2626",
+      navBarHamburger: "#f1f5f9",
+    },
   },
   plugins: [],
-}
-
+};
