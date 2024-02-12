db65c1aa93b846d33ec8843754ae48af9c99a8ec
LuiiVuitton <Luis1134@icloud.com>
2024-02-12 00:59:28 -0500

    added about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,19 +1,44 @@
+import image from "../assets/LexusImage.jpg";
+
 function About() {
   return (
-    <div className="bg-burgandy py-8 text-center">
-      <h1 className="text-white text-3xl font-bold mb-4">What we do?</h1>
-      <p className="text-white leading-relaxed mx-auto max-w-3xl">
-        Welcome to Alfaro Auto Detailing, where we transform your car into a
-        showroom masterpiece. Our expert team of auto detailing specialists is
-        dedicated to restoring and enhancing the beauty of your vehicle, inside
-        and out. From thorough exterior washes and meticulous paint corrections
-        to luxurious interior treatments, we offer a comprehensive range of
-        services tailored to meet your car's unique needs. Using top-of-the-line
-        products and cutting-edge techniques, we ensure that every inch of your
-        vehicle receives the attention it deserves, leaving it looking pristine
-        and protected. Experience the ultimate in automotive luxury with
-        [Business Name], where excellence meets craftsmanship.
-      </p>
+    <div id="About" className="bg-sectionLight text-black py-10">
+      <div className="container mx-auto px-5">
+        <div className="flex flex-col lg:flex-row items-center justify-center lg:justify-between">
+          <div className="lg:w-1/2 lg:order-1 mt-10 lg:mt-0 pr-20 pb-10">
+            <img
+              src={image}
+              alt="About Me"
+              className="w-full rounded-lg shadow-lg"
+            />
+          </div>
+
+          <div className="lg:w-1/2 lg:order-2 px-4 lg:px-0">
+            {/* Change order to 2 for the image column on larger screens */}
+            <h1 className="text-3xl lg:text-3xl font-bold mb-4">
+              "A clean Car gives more Beauty"
+            </h1>
+            <p className="text-lg mb-4">
+              A family owned business based in Gaithersburg, MD.
+            </p>
+            <p className="mb-4">
+              Welcome to Alfaro Auto Detail Shop, where our family owned
+              business is committed to restoring your vehicle's beauty, inside
+              and out. With our 10+ years of experience we promise to deliver
+              amazing results and customer satisfaction.
+            </p>
+            <div className="text-center lg:text-left">
+              <button className="bg-white text-burgandy font-bold py-2 px-6 rounded-full shadow-lg hover:bg-pink-400 hover:text-white hover:scale-105 transition duration-300 mr-4">
+                Contact Us
+              </button>
+              {/* Added hover:scale-105 class to increase size on hover */}
+              <button className="bg-lightBlue text-white font-bold py-2 px-6 rounded-full shadow-lg hover:bg-pink-400 hover:text-white hover:scale-105 transition duration-300">
+                Book Now
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,17 @@
 function Home() {
   return (
-    <div className="bg-gradient-to-r from-purple-400 to-pink-500 h-screen flex flex-col justify-center items-center">
+    <div className="bg-white flex flex-col justify-center items-center pb-20 pt-10">
       <div className="text-center 	">
         <p className="text-lg text-black mb-8">Welcome to</p>
         <h1 className="text-5xl font-bold text-black mb-4">
           Alfaro's Auto Detailing
         </h1>
-        <p className="text-lg text-black mb-8">
-          "A clean car gives more beauty"
-        </p>
+        <p className="text-lg text-black mb-8"></p>
         <button className="bg-burgandy text-white font-bold py-2 px-6 rounded-full shadow-lg hover:bg-pink-400 hover:text-white transition duration-300">
           Book now
         </button>
       </div>
-      <div className="absolute bottom-0 mb-6 text-black text-center">
+      <div className="pt-10 text-black text-center">
         <p>Scroll down to learn more</p>
         <svg
           className="w-6 h-6 mx-auto mt-2 animate-bounce text-black"
